3a1cb937d50771c31cf36ac8b0d5734cdb97929b
jacksonhedge <jackson@hedgepayments.com>
2025-08-01 01:40:05 -0400

    Remove super admin restriction for college map - make accessible to all admins

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -119,8 +119,7 @@ const AdminLayout = ({ adminLevel }) => {
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7" />
             </svg>
-          ),
-          adminOnly: true
+          )
         },
         { 
           name: 'Calendar', 
@@ -268,6 +267,7 @@ const AdminLayout = ({ adminLevel }) => {
             <Route path="brackets" element={<BracketManagementPage />} />
             <Route path="partners" element={<AdminPartnersPage />} />
             <Route path="sales" element={<SalesPipelinePage />} />
+            <Route path="map" element={<AdminMapPage />} />
             
             {/* Super admin only routes */}
             {adminLevel === AdminLevel.SUPER && (
@@ -275,7 +275,6 @@ const AdminLayout = ({ adminLevel }) => {
                 <Route path="colleges" element={<AdminCollegesPage />} />
                 <Route path="fraternities" element={<AdminFraternitiesPage />} />
                 <Route path="calendar" element={<AdminCalendarPage onLogout={handleLogout} />} />
-                <Route path="map" element={<AdminMapPage />} />
               </>
             )}
             
